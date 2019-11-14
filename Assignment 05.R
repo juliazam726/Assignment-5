2e4cbcefa4b0e9d377fab5d6501d2ad3b6c58cf0
Julia <jez223@lehigh.edu>
2019-11-14 15:14:03 -0500

    Assignment 05

---
+++ Assignment 05.R
@@ -0,0 +1,13 @@
+ptm <- proc.time()
+DF <- read.csv("DRFAPGACBN.csv")
+CSV_READ_TIME <- (proc.time() - ptm)
+CSV_READ_TIME
+
+if (!require("data.table")) install.packages("data.table")
+library("data.table")
+
+ptm <- proc.time()
+DF <- fread("DRFAPGACBN.csv", header="auto", 
+            data.table=FALSE)
+FREAD_READ_TIME <- (proc.time() - ptm)
+FREAD_READ_TIME
